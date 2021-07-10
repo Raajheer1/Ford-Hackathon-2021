e87a1b0d546253d2e10cfc06da7f369d3a3b0ec4
Raajheer1 <41763998+Raajheer1@users.noreply.github.com>
2021-07-10 11:29:24 -0500

    Add .idea to gitignore

--- routes/db/user.js
+++ routes/db/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { MongoClient } = require('mongodb');
-
 const router = express.Router();
 
+const url = 'mongodb://'
 
 //Authenticate
 app.get("login", (req, res) => {
